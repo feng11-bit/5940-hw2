daf8b477ccf7782a51da2385311cc1b1b596454a
fengxiao <feng11@seas.upenn.edu>
2026-02-25 14:17:00 -0500

    MyNode.java

--- src/MyNode.java
+++ src/MyNode.java
@@ -1,2 +1,37 @@
-public class MyNode {
+public class MyNode<T> {
+    private T item;
+    private MyNode<T> left;
+    private MyNode<T> right;
+    private MyNode<T> parent;
+
+    //constructor
+    public MyNode(T item){
+        this.item = item;
+        this.left = null;
+        this.right = null;
+        this.parent = null;
+    }
+    //getter
+    public T getItem() {
+        return item;
+    }
+    public MyNode<T> getLeft() {
+        return left;
+    }
+    public MyNode<T> getRight() {
+        return right;
+    }
+    public MyNode<T> getParent() {
+        return parent;
+    }
+    //setter
+    public void setItem(T item) {
+        this.item = item;
+    }
+    public void setLeft(MyNode<T> left) {
+        this.left = left;
+    }
+    public void setRight(MyNode<T> right) {
+        this.right = right;
+    }
 }
